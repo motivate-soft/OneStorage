fa0a451ad1400c4095b0f52ed86016eb1bea6a39
robert-upwork <robyop.eyeco@gmail.com>
2020-08-31 16:26:04 +0700

    Mobile pages

---
+++ resources/views/mobile/layouts/footer.blade.php
@@ -0,0 +1,349 @@
+<footer class="w-full static pt-10 bottom-0 bg-grey">
+    <div class="pb-5" style="margin-left:100px; margin-right:100px; ">
+        <div class="grid lg:grid-cols-3 lg:gap-4 sm:grid-cols-1 sm:min-w-full">
+            <div style="margin-right: 30px;">
+                <div class="mt-2 ml-6">
+                    <h2 class=" text-lg my-8 font_15">關於 <b>One Storage.</b></h2>
+                    <div class=" text-justify my-4 leading-5 font_13">至尊迷你倉為金朝陽集團屬下業務(股票代號：
+                        00878.HK)。<br>
+                        <br>
+
+                        至尊迷你倉致力為每位客戶打造最安全及最可靠的倉存
+                        空間，實力雄厚，自置物業。分店網絡遍佈港、九、新
+                        界，並且積極不斷擴充。 特設多種不同呎碼獨立迷你
+                        倉，任君選擇。<br>
+                        <br>
+
+                        2.4米闊的走廊配以完善的裝置設備，完全符合消防指
+                        米闊的走廊配以完善的裝置設備，完全符合消防指
+                        統，24小時自助式儲存，支援現金、EPS、支票、信用卡、
+                        轉數快及免息分期付費，優質的儲存環境及親切專業的
+                        客戶服務，用心為每位客戶提升生活空間質素，承傳尊
+                        貴享受典範。
+                    </div>
+                    <h2 class="my-8 font_15">職位空缺</h2>
+
+                </div>
+            </div>
+            <div style="margin-left: -30xp; margin-right:-30px;">
+                <div class="mt-2">
+                    <h2 class="my-8 ml-1 font_15">連絡我們</h2>
+                    <div class="grid grid-cols-6 gap-4 font_13 ml-1">
+                        <div class="flex col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-5 xl:col-span-2...">
+                            <img class="fill-current mr-4" src="{{asset('images/footer/icons8-phone-50@2x.png')}}" />
+                            <span class="self-center text-primary">(852) 2111 2636</span>
+                        </div>
+                        <div class="flex col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-5 xl:col-span-2...">
+                            <img class="fill-current mr-4" src="{{asset('images/footer/icons8-email-open-50@2x.png')}}" />
+                            <span class="self-center text-primary">cs@onestorage.com.hk</span>
+                        </div>
+                        <div class="flex col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-5 xl:col-span-2...">
+                            <img class="fill-current mr-4" src="{{asset('images/footer/icons8-whatsapp-50@2x.png')}}" />
+                            <span class="self-center text-primary">(852) 5118 8503</span>
+                        </div>
+                        <div class="flex col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-5 xl:col-span-2...">
+                            <img class="fill-current mr-4" src="{{asset('images/footer/icons8-home-50@2x.png')}}" />
+                            <span class="self-center text-primary">查看分店地址</span>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="grid grid-cols-2  mt-8 ">
+                    <div class="col-span-1 ">
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex font_13">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">屯門</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5 ">
+                                <p class="font_13">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_13">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_13">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex font_13">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">荃灣</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_13">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_13">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_13">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex font_13">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">火炭</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_13">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_13">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_13">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">新蒲崗</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_14">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_14">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_14">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">長沙灣</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_14">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_14">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_14">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                    <div class="col-span-1">
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex font_13">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">葵芳</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_13">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_13">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_13">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex font_13">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">葵涌</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_13">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_13">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_13">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex font_13">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">葵興</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_13">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_13">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_13">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex font_13">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">青衣</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_13">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_13">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_13">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="accordion relative appearance-none items-center">
+                            <div class="flex">
+                                <img class="fill-current mr-4" src="{{asset('images/footer/Group 22.png')}}" />
+                                <span class="self-center text-primary">柴灣</span>
+                            </div>
+                        </div>
+                        <div class="panel flex">
+                            <img class="fill-current" style="height: 50px;" src="{{asset('images/footer/Artboard 1@72x-8@2x.png')}}" />
+                            <div class=" leading-5">
+                                <p class="font_14">
+                                    電話 : <span>2374-2022</span>
+                                </p>
+                                <p class="font_14">
+                                    電郵 : <span>cs@onestorage.com.hk</span>
+                                </p>
+                                <p class="font_14">
+                                    地址 : <span>新界屯門新益里3號通明工業大廈
+                                        4/5樓</span>
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </div>
+            <div style="margin-left: 25px;">
+                <div class="mt-2">
+                    <h2 class=" text-lg my-8">連絡我們</h2>
+                    <div class="md:flex  rounded-lg mt-2 pb-5">
+                        <img class="h-24 w-24 md:h-28 md:w-28 mx-auto md:mx-0 md:mr-4" src="{{asset('images/footer/Image 17@2x.png')}}">
+                        <div class="text-center md:text-left">
+                            <h2 class="text-sm text-justify leading-4 font_13">
+                                黃竹坑新店快閃優惠　低至6折優惠
+                                震撼筍價HK$300起即可入手<br>
+                                <br>
+                                黃竹坑分店全新開業，推出快閃驚喜優
+                                ！顧客可享低至6折優惠，以震撼筍價
+                                HK$300起即可入手！你仲唔快啲黎搵我
+                                哋！！
+                            </h2>
+                        </div>
+
+                    </div>
+                    <div class="md:flex  rounded-lg mt-2 pb-5">
+                        <img class="h-24 w-24 md:h-28 md:w-28 mx-auto md:mx-0 md:mr-4" src="{{asset('images/footer/Image 17@2x.png')}}">
+                        <div class="text-center md:text-left">
+                            <h2 class="text-sm text-justify leading-4 font_13">
+                                黃竹坑新店快閃優惠　低至6折優惠
+                                震撼筍價HK$300起即可入手<br>
+                                <br>
+                                黃竹坑分店全新開業，推出快閃驚喜優
+                                ！顧客可享低至6折優惠，以震撼筍價
+                                HK$300起即可入手！你仲唔快啲黎搵我
+                                哋！！
+                            </h2>
+                        </div>
+
+                    </div>
+                    <div class="md:flex  rounded-lg mt-2 pb-5">
+                        <img class="h-24 w-24 md:h-28 md:w-28 mx-auto md:mx-0 md:mr-4" src="{{asset('images/footer/Image 17@2x.png')}}">
+                        <div class="text-center md:text-left">
+                            <h2 class="text-sm text-justify leading-4 font_13">
+                                黃竹坑新店快閃優惠　低至6折優惠
+                                震撼筍價HK$300起即可入手<br>
+                                <br>+
+                                黃竹坑分店全新開業，推出快閃驚喜優
+                                ！顧客可享低至6折優惠，以震撼筍價
+                                HK$300起即可入手！你仲唔快啲黎搵我
+                                哋！！
+                            </h2>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script>
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            // this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                panel.style.border = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                // panel.style.border = "1px solid #DCDCDC";
+            }
+        });
+    }
+    //Javascript to toggle the menu
+    document.getElementById('nav-toggle').onclick = function() {
+        document.getElementById("nav-content").classList.toggle("hidden");
+    }
+</script>
