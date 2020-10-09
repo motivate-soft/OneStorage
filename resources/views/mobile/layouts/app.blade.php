9979787cb37cda6a57409753315547cc17ba09a6
robert-upwork <robyop.eyeco@gmail.com>
2020-10-09 14:56:02 +0700

    feedback

--- resources/views/mobile/layouts/app.blade.php
+++ resources/views/mobile/layouts/app.blade.php
@@ -10,6 +10,25 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     @yield('title')
+
+<!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1306853613000826');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="display:none"
+             src="https://www.facebook.com/tr?id=1306853613000826&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link href="{{asset('css/font_size.css')}}" rel="stylesheet">
@@ -115,7 +134,7 @@
 
 <body class="bg-white h-screen antialiased leading-none" ondragstart="return false;" ondrop="return false;">
 
-    @yield('accessory')
+
     <!-- back to top floating button -->
     <button onclick="topFunction()" id="floatBT" title="Go to top">
         <i class="fa fa-arrow-up"></i>
