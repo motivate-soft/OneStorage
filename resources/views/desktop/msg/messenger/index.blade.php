12b236559856eff9df06bdb48935ea8141d59d22
robert-upwork <robyop.eyeco@gmail.com>
2020-10-31 01:53:02 +0700

    20201031

--- resources/views/desktop/msg/messenger/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('msg.layouts.master')
-
-@section('content')
-    @include('msg.messenger.partials.flash')
-
-    @each('msg.messenger.partials.thread', $threads, 'thread', 'msg.messenger.partials.no-threads')
-@stop
